3b1169e5507e985cccabf67ef84f0d13f70aa05a
H.P. Grahsl <grahslhp@gmail.com>
2023-01-02 21:00:23 +0100

    refactor code and folder structure to better support multiple journey in the repo
    - introduce config file for a journey regarding potential emojis to mirror, authors, repo url link
    - rename files to better reflect intention of the different artefacts involved in the svg generation
    - adapt and extend README to reflect these changes

--- Convert.java
+++ Convert.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -7,9 +9,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Convert {
 
@@ -22,26 +26,38 @@ public class Convert {
                                    }
                                    """;
 
-    private static final List<String> emojisToMirror = Arrays.asList("🚂", "🚡", "🚴‍♂️", "⛵");
+    private static final String CONFIG_KEY_EMOJIS = "emojis.to.mirror";
+    private static final String CONFIG_KEY_TWITTER = "twitter.handles";
+    private static final String CONFIG_KEY_REPO = "journey.short.url";
 
     public static void main(String... args) throws Exception {
         if (args == null || args.length != 1) {
             System.out.println("Usage: java Convert.java <journey>");
             return;
         }
-
         String journey = args[0];
-
+        System.out.println("Processing "+journey+" journey");
+        Properties config = readConfig(journey);
         List<Entry> entries = getEntries(journey);
-        Path output = createOutputFile(journey, entries);
-
+        Path output = createOutputFile(journey, entries, config);
         System.out.println("Created " + output);
+    }
+
+    private static Properties readConfig(String journey) throws Exception {
+        if(!Files.exists(Path.of(journey+"/config.properties"))) {
+            return new Properties();
+        }
+        try (InputStreamReader isr = new FileReader(new File(journey+"/config.properties"), StandardCharsets.UTF_8)) {
+            Properties properties = new Properties();
+            properties.load(isr);
+            return properties;
+        }
     }
 
     private static List<Entry> getEntries(String journey) throws Exception {
         List<Entry> entries = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new File(journey +  "-links.csv"))) {
+        try (Scanner scanner = new Scanner(new File(journey +  "/links.csv"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
 
@@ -55,9 +71,9 @@ public class Convert {
         return entries;
     }
 
-    private static Path createOutputFile(String journey, List<Entry> entries) throws Exception {
-        Path source = Paths.get(journey + ".svg");
-        Path target = Paths.get("docs/" + journey + ".svg");
+    private static Path createOutputFile(String journey, List<Entry> entries, Properties config) throws Exception {
+        Path source = Paths.get(journey + "/exported.svg");
+        Path target = Paths.get("docs/" + journey + "/generated.svg");
         Charset charset = StandardCharsets.UTF_8;
 
         String content = new String(Files.readAllBytes(source), charset);
@@ -68,18 +84,37 @@ public class Convert {
 
         // inject styles
         content = content.replace("</style>", styles + "</style>");
-
-        for(String emoji : emojisToMirror) {
-            Matcher matcher = Pattern.compile(">" + emoji).matcher(content);
-            System.out.println("Found: " + matcher.find() + " " + matcher.group());
-            content = matcher.replaceAll(" transform=\"scale(-1, 1)\">" + emoji);
+        
+        //mirror emojis where applicable according to configuration
+        String emojiConfig = config.getProperty(CONFIG_KEY_EMOJIS);
+        if(emojiConfig != null && !emojiConfig.isBlank()) {
+            List<String> emojisToMirror = Arrays.stream(SEPARATOR.split(emojiConfig))
+                                            .map(String::trim).collect(Collectors.toList());
+            for(String emoji : emojisToMirror) {
+                System.out.println("Searching occurrences of emoji "+emoji+ " to mirror");
+                Matcher matcher = Pattern.compile(">" + emoji).matcher(content);
+                System.out.println("Found: " + matcher.find() + " " + matcher.group());
+                content = matcher.replaceAll(" transform=\"scale(-1, 1)\">" + emoji);
+            }
         }
-
+        
+        // replace text with proper links where applicable according to configuration  
         // twitter handles
-        content = content.replace("@gunnarmorling", "<a href=\"https://twitter.com/gunnarmorling\" target=\"_top\">@gunnarmorling</a>");
-        content = content.replace("@hpgrahsl", "<a href=\"https://twitter.com/hpgrahsl\" target=\"_top\">@hpgrahsl</a>");
-        // github repo
-        content = content.replace("https://bit.ly/journey4kafka", "<a href=\"https://bit.ly/journey4kafka\" target=\"_top\">https://bit.ly/journey4kafka</a>");
+        String twitterHandleConfig = config.getProperty(CONFIG_KEY_TWITTER);
+        if(twitterHandleConfig != null && !twitterHandleConfig.isBlank()) {
+            List<String> twitterHandles = Arrays.stream(SEPARATOR.split(twitterHandleConfig))
+                                            .map(String::trim).collect(Collectors.toList());
+            for(String twitterHandle : twitterHandles) {
+                System.out.println("Replace twitter handle "+twitterHandle+" with HTML link to profile");
+                content = content.replace(twitterHandle, "<a href=\"https://twitter.com/"+twitterHandle+"\" target=\"_top\">"+twitterHandle+"</a>");
+            }
+        }
+        //repo URL
+        String repoConfig = config.getProperty(CONFIG_KEY_REPO);
+        if(repoConfig != null && !repoConfig.isBlank()) {
+            System.out.println("Replace repo URL with HTML link "+repoConfig+" with profile URL");
+            content = content.replace(repoConfig, "<a href=\""+repoConfig+"\" target=\"_top\">"+repoConfig+"</a>");
+        }
         Files.write(target, content.getBytes(charset));
 
         return target;
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# A Great Day Out With...
+# A Great Day Out With ...
 
-Curated learning journeys for technologies like [Apache Kafka](https://a-great-day-out-with.github.io/kafka.html).
+Curated learning journeys for technologies like:
+- [Apache Kafka](https://a-great-day-out-with.github.io/kafka.html)
+- TODO: your learning journeys for XYZ ;-)
 
 ## Philosophy
 
@@ -13,13 +15,43 @@ The contents of a journey is in flux, i.e. items may be added and removed over t
 
 Suggestions for additions are highly appreciated, but the final decision about inclusion is made by the core maintainers of a journey.
 
-## Build
+## How does it work?
 
-* Load _kafka.excalidraw_ into [excalidraw.com](https://excalidraw.com), export as SVG to _kafka.svg_.
-* Add/adjust links in _kafka-links.csv_
-* Process the SVG file: `java Convert.java kafka`
+All journey specific artefacts are supposed to live in their own separate folders, see e.g. the `kafka` folder.
+The following file naming conventions are important:
 
-Then open _docs/kafka.html_ in your browser.
+- `illustration.exaclidraw` **[mandatory]**: The main artefact for drawing and artwork for a journey and can be created from scratch or loaded using [excalidraw.com](https://excalidraw.com)
+- `exported.svg` **[mandatory]**: The SVG version of the main artefact created by manually exporting your illustration file above from the excalidraw web UI
+- `links.csv` **[mandatory]**: This file stores all URLs behind each textual element in the learning journey. Matching of text elements (drawing) and links (CSV file) is done by simple string replacements so make sure to get your names spelled correctly in both, the excalidraw and CSV file.
+- `config.properties` _[optional]_: This file allows to configure specific "post-processing" actions for generating the output artefacts. Currently the following three properties - all of which are optional themselves - can be defined and are picked up by the `Converter.java`
+
+    ```properties
+    emojis.to.mirror=🚂;🚡;🚴‍♂️;⛵
+    twitter.handles=@gunnarmorling;@hpgrahsl
+    journey.short.url=https://bit.ly/journey4kafka
+    ```
+
+    - `emojis.to.mirror`: allows to define a list of emojis which are used in the drawing and should get mirrored horizontally if needed
+    - `twitter.handles`: in case author information is given by textual twitter handles, those handles can be replaced with links to profile URLs if needed
+    - `journey.short.url`: if applicable a textual tiny URL referring to the project repository can be replaced with the corresponding HTML link
+
+
+## How to contribute to an existing journey
+
+### The pretty simple way:
+
+1. add/modify links in the `<journey_folder>/links.csv` file
+2. create a PR for this change
+
+_NOTE: The core maintainers will do their best to make all changes to graphical artefacts (excalidraw + SVG) that are most likely needed due to changes in the `links.csv` file._
+
+### The more involved way:
+
+1. add/modify links in the `<journey_folder>/links.csv` file and create a PR
+2. load `<journey_folder>/illustration.excalidraw` into [excalidraw.com](https://excalidraw.com), make graphical changes if needed and export an SVG file into `<journey_folder>/exported.svg`
+3. run java converter tool `java Convert.java <journey>` which processes the `<journey_folder>/exported.svg` and writes the final result into `docs/<journey_folder>/generated.svg`
+4. open `docs/<journey_folder>/index.html` in your browser to verify if your changes are properly  reflected in the generated SVG file that's embedded in this HTML document.
+5. create a PR for your change which should usually contain 4 changed files, namely: `<journey_folder>/links.csv`, `<journey_folder>/illustration.excalidraw`, `<journey_folder>/exported.svg`, `docs/<journey_folder>/generated.svg`
 
 ## License
 
